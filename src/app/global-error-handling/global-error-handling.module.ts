cb7919d19358c577f0684120118661899cc61742
Temikhelidze <mixelidzet@gmail.com>
2022-11-28 01:12:21 +0400

    feature - page not found

---
+++ src/app/global-error-handling/global-error-handling.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GlobalErrorHandlingRoutingModule } from './global-error-handling-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, GlobalErrorHandlingRoutingModule],
+})
+export class GlobalErrorHandlingModule {}
