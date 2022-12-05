2a136f374370d471feee1c9a5da96a8a42d4e2ca
Temikhelidze <mixelidzet@gmail.com>
2022-12-05 13:16:17 +0400

    calendar

--- src/app/features/calendar/calendar.module.ts
+++ src/app/features/calendar/calendar.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarComponent } from './component/calendar.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [CalendarComponent],
-  imports: [CommonModule, CalendarRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    CalendarRoutingModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,
+  ],
 })
 export class CalendarModule {}
--- src/app/features/calendar/component/calendar.component.ts
+++ src/app/features/calendar/component/calendar.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -10,13 +15,28 @@ import {
   DateFormGroup,
 } from '../interfaces/calendar-formgroup.interface';
 import { combineLatest, Subject, takeUntil } from 'rxjs';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import {
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+  faChevronUp,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CalendarComponent implements OnInit, OnDestroy {
+  readonly leftArrowIcon: IconDefinition = faChevronLeft;
+  readonly rightArrowIcon: IconDefinition = faChevronRight;
+  readonly upArrowIcon: IconDefinition = faChevronUp;
+  readonly downArrowIcon: IconDefinition = faChevronDown;
+
+  public displayCalendarPopup: boolean = false;
+
   form: FormGroup<CalendarFormGroup> = this.fb.group<CalendarFormGroup>({
     firstDate: this.fb.group<DateFormGroup>({
       day: this.fb.control('', [Validators.required]),
@@ -55,8 +75,7 @@ export class CalendarComponent implements OnInit, OnDestroy {
         this.currentDateControl.value?.getFullYear()
     ),
     Number(
-      this.firstMonthControl.value ||
-        this.currentDateControl.value?.getMonth() + 1
+      this.firstMonthControl.value || this.currentDateControl.value?.getMonth()
     ),
     Number(
       this.firstDayControl.value || this.currentDateControl.value?.getDate()
@@ -68,8 +87,7 @@ export class CalendarComponent implements OnInit, OnDestroy {
         this.currentDateControl.value?.getFullYear()
     ),
     Number(
-      this.secondMonthControl.value ||
-        this.currentDateControl.value?.getMonth() + 1
+      this.secondMonthControl.value || this.currentDateControl.value?.getMonth()
     ),
     Number(
       this.secondDayControl.value || this.currentDateControl.value?.getDate()
@@ -109,10 +127,85 @@ export class CalendarComponent implements OnInit, OnDestroy {
       });
   }
 
-  getCalendarForUi(date: Date) {}
+  monthWeeksViewForUi(date: Date): { [key: string]: (number | null)[] } {
+    let firstOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+    let lastOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    let datesInWeeks: { [key: string]: (number | null)[] } = {};
+    let weekIterator: number = 1;
+
+    for (let i = 1; i <= lastOfMonth.getDate(); i++) {
+      if (datesInWeeks[weekIterator]) {
+        datesInWeeks[weekIterator].push(i);
+      } else {
+        datesInWeeks[weekIterator] = [i];
+      }
+      if (firstOfMonth.getDay() % 7 === 0) {
+        if (i < 8) {
+          let diff = 6 - datesInWeeks[weekIterator].length;
+          let lastOfPreviousMonthDate = new Date(
+            date.getFullYear(),
+            date.getMonth() - 1,
+            0
+          ).getDate();
+          while (diff >= 0) {
+            // datesInWeeks[weekIterator].unshift(lastOfPreviousMonthDate);
+            datesInWeeks[weekIterator].unshift(null);
+            --lastOfPreviousMonthDate;
+            --diff;
+          }
+        }
+        weekIterator++;
+      }
+      firstOfMonth.setDate(firstOfMonth.getDate() + 1);
+    }
+
+    return datesInWeeks;
+  }
+
+  compareTwoDateWithYearMonthDate(
+    date1: Date,
+    date2: Date,
+    getDate1?: number | null,
+    getDate2?: number | null
+  ): boolean {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      (getDate1 !== undefined ? getDate1 : date1.getDate()) ===
+        (getDate2 !== undefined ? getDate2 : date2.getDate())
+    );
+  }
 
   ngOnDestroy() {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
+  }
+
+  getPrefix(
+    tempValue: number | string,
+    type: 'hour' | 'minutes' | 'seconds'
+  ): string | number {
+    let value = Number(tempValue);
+    if (value || value === 0) {
+      if (type === 'hour' || type === 'minutes' || type === 'seconds') {
+        return value >= 0 && value < 10 ? `0${value}` : value;
+      } else return value;
+    } else return tempValue;
+  }
+
+  showCalendarPopup() {
+    this.displayCalendarPopup = true;
+  }
+
+  hideCalendarPopup() {
+    this.displayCalendarPopup = false;
+    console.log(this.firstDate);
+    console.log(this.secondDate);
+  }
+
+  selectDate(dateToSetValue: Date, value: number | null) {
+    if (value) {
+      dateToSetValue.setDate(value);
+    }
   }
 }
