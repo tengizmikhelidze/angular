7e263061dea85506674ceb1491be41a09e110751
Tengiz Mikhelidze <mixelidzet@gmail.com>
2023-02-08 12:59:32 +0400

    check version

--- src/app/core/load-config-json/models/app-config.interface.ts
+++ src/app/core/load-config-json/models/app-config.interface.ts
@@ -1,4 +1,6 @@
 export interface AppConfig {
   baseHref: string;
   apiUrl: string;
+
+  appVersion: string;
 }
--- src/environment/enviornment.ts
+++ src/environment/enviornment.ts
@@ -3,4 +3,5 @@ import { AppConfig } from '../app/core/load-config-json/models';
 export const environment: AppConfig = {
   baseHref: '/',
   apiUrl: '/',
+  appVersion: require('../../package.json').version,
 };
