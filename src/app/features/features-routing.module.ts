582f0709a2b3461da095b005cbe9193149b40f70
Tengiz Mikhelidze <mixelidzet@gmail.com>
2023-03-16 03:51:01 +0400

    feature - redirect

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
       {
         path: '',
         pathMatch: 'full',
-        redirectTo: 'inputs',
+        redirectTo: 'QR',
       },
       {
         path: 'calendar',
